942701d405cdf6b04cc0fcb6cf6a42cc5e68c65b
GaipovKhamid <gaipovkhamid65@gmail.com>
2025-06-02 14:37:04 +0500

    first-lesson

--- src/lessons/FindMinNumbInArray.java
+++ src/lessons/FindMinNumbInArray.java
@@ -5,5 +5,7 @@ In this video lesson I create an algorithm for find minimum number in massive.
  */
 
 public class FindMinNumbInArray {
-
+    public static void main(String[] args) {
+        System.out.println("d");
+    }
 }
